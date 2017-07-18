eba59442468e080e8527f41055aafad1fcfcedca
Richard <richard7.lehman@gmail.com>
2017-07-18 09:27:59 -0400

    Organized gulpfile.js into multiple smaller, highly focused files.

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nestedcss = require('postcss-nested'),
+cssImport = require('postcss-import');
+
+gulp.task('styles', function () {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssvars, nestedcss, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
